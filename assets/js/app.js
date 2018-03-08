56f1af610de77485cf942cd4ef571b7dc9405472
Massienderella <massiel.ams@gmail.com>
2018-03-08 10:28:53 -0300

    archivos funcionalidades

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
 });
+
+$(".about").smoove({
+  offset  : '15%',
+});
+
+$(".waves").smoove({
+  offset  : '15%',
+});
+
+$(".my-skills").smoove({
+  offset  : '15%',
+});
