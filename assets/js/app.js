e3287198bc06e0dc0b24fdfe60fe52e5b2956b93
Massienderella <massiel.ams@gmail.com>
2018-03-19 10:23:54 -0300

    carrusel websites

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,27 @@ $(".about").smoove({
   offset  : '15%',
 });
 
-$(".waves").smoove({
-  offset  : '15%',
-});
-
 $(".my-skills").smoove({
   offset  : '15%',
 });
+
+function hoverSkills(){
+  // Mostrar al Hover
+  $('.skill-cont').hover(function() {
+    $(this).children('img').css({
+      'opacity': '0',
+      'transition': 'ease all 0.5s',
+    });
+    $(this).children('figcaption').css({
+      'opacity': '1',
+    });
+  },
+  function() {
+    $(this).children('img').css({
+      'opacity': '1',
+    });
+    $(this).children('figcaption').css({
+      'opacity': '0',
+    });
+  });
+}
